d1658f29f39aaf7f4a6ba9a07c7e657c14a0daaa
Cecilia <cecicassab@gmail.com>
2025-11-21 16:31:51 -0300

    fix smells tirando método deprecated Close #32, #31, #29

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,22 @@
+import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { errorHandler } from './middleware/errorHandler';
 
 class App {
     public server: express.Application;
 
     constructor() {
         this.server = express();
-        this.middlewares();
         this.routes();
+        this.middlewares();
     }
 
     private middlewares(): void {
         this.server.use(express.json());
         this.server.use(cors()); 
+        this.server.use(errorHandler);
     }
 
     private routes(): void {
